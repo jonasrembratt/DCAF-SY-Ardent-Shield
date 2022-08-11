831e695b49d8da1cf5374c9c82a4c50b79656ac0
Jonas "Wife" Rembratt <jonas@rembratt.com>
2022-08-11 19:46:32 +0200

    Improved AirPolicing script / replaced A380 > B747
    - Intercept script now takes 'options' object instead of individual args
    - Intercept script can now send description of intruder to interceptors

--- SY_ardent_shield-1-05-02-14.lua
+++ SY_ardent_shield-1-05-02-14.lua
@@ -1,7 +1,7 @@
 local menuCommands = {}
 local menuGMHariri = "Hariri assassination"
-local menuGM_Civ_FR_A380_START = "START A-380 Sydney-Paris"
-menuGM_Civ_FR_A380_DIVERT = "DIVERT French A-380 Sydney > Paris"
+local menuGM_Civ_FR_A380_START = "START B747 Sydney > Paris"
+menuGM_Civ_FR_A380_DIVERT = "DIVERT French B747 Sydney > Paris"
 local menuGM_Civ_IS_Cessna_START = "START Isreaeli Cessna"
 menuGM_Civ_IS_Cessna_DIVERT = "DIVERT Isreaeli Cessna"
 local menuGM_Mil_SY_AN26_TPT_START = "START SY AN26 TPT Abu-al-Duhur > Mezzeh"
@@ -35,8 +35,8 @@ end
 local MenuGM = MENU_COALITION:New(coalition.side.BLUE, "Game Master")
   menuCommands[menuGMHariri] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, menuGMHariri, MenuGM, RunHariri)
   local MenuGMAir = MENU_COALITION:New(coalition.side.BLUE, "Air Traffic", MenuGM)
-    menuCommands[menuGM_Civ_FR_A380_START] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, menuGM_Civ_FR_A380_START, MenuGMAir, SetFlag, "civ_fr_a380_start", true, menuGM_Civ_FR_A380_START)
-    menuCommands[menuGM_Civ_FR_A380_DIVERT] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, menuGM_Civ_FR_A380_DIVERT, MenuGMAir, SetFlag, "civ_fr_a380_divert", true, menuGM_Civ_FR_A380_DIVERT)
+    menuCommands[menuGM_Civ_FR_A380_START] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, menuGM_Civ_FR_A380_START, MenuGMAir, SetFlag, "civ_fr_b747_start", true, menuGM_Civ_FR_A380_START)
+    menuCommands[menuGM_Civ_FR_A380_DIVERT] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, menuGM_Civ_FR_A380_DIVERT, MenuGMAir, SetFlag, "civ_fr_b747_divert", true, menuGM_Civ_FR_A380_DIVERT)
     menuCommands[menuGM_Civ_IS_Cessna_START] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, menuGM_Civ_IS_Cessna_START, MenuGMAir, SetFlag, "civ_is_cessna_start", true, menuGM_Civ_IS_Cessna_START)
     menuCommands[menuGM_Civ_IS_Cessna_DIVERT] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, menuGM_Civ_IS_Cessna_DIVERT, MenuGMAir, SetFlag, "civ_is_cessna_divert", true, menuGM_Civ_IS_Cessna_DIVERT)
     menuCommands[menuGM_Mil_SY_AN26_TPT_START] = MENU_COALITION_COMMAND:New(coalition.side.BLUE, menuGM_Mil_SY_AN26_TPT_START, MenuGMAir, SetFlag, "mil_sy_an26_start", true, menuGM_Mil_SY_AN26_TPT_START)
